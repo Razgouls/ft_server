fa3925519a374d270396009d74f1e562df884fa1
eoliveir <elie.oliveir@gmail.com>
2021-01-22 09:08:58 +0100

    ft_server

--- srcs/mysql.sh
+++
@@ -1,6 +0,0 @@
-service mysql start
-echo "CREATE DATABASE wordpress;" | mysql -u root
-echo "CREATE USER 'test'@'localhost';" | mysql -u root
-echo "SET password FOR 'test'@'localhost' = password('password');" | mysql -u root
-echo "GRANT ALL ON testdb.* TO 'test'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
